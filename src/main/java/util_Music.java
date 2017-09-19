eecc9df993bf7cdd82eb5fafec8fe42aa7c6d672
theRVR3ND <kelvinwpeng@gmail.com>
2017-09-19 14:55:36 -0400

    Some Progress
    Got some work done on music generation.

--- src/main/java/util_Music.java
+++ src/main/java/util_Music.java
@@ -12,19 +12,36 @@ import java.util.*;
 
 public class util_Music{
    
+   //********STATIC CONSTANTS********//
    
+   private static final byte MAJOR = 0,
+                             MINOR = 1,
+                             BLUES = 2,
+                          HARMONIC = 3,            //Harmoic minor
+                        NUM_SCALES = 4;
    
-   public static byte[][] generateSong(byte difficulty, byte numPlayer, short seed){
+   private static final byte[] KEYS = new byte[] {0, 1, 2, 3, 4, 5, 6};
+   
+   private static final byte[] INSTRUMENTS = new byte[] {1, 28, 36, 35, 31, 56};
+   
+   //Run
+   public static byte[][] generateSong(byte difficulty, short seed){
       //Make a random number genrator
       Random rand = new Random(seed);
       
       //Generate song parameters
-      //final byte bps
-      //final byte scale
-      //final byte key
+      final short bpm = (short)((difficulty / 100.0) * rand.nextDouble() * 170 + 60);
+      final short songLength = (short)((rand.nextDouble() * 2.5 + 0.5) * bpm);
+      final byte scale = (byte)(rand.nextDouble() * NUM_SCALES);
+      final byte key = KEYS[(int)(rand.nextDouble() * KEYS.length)];
       
-      //Find song length (number of beats)
-      byte[][] gen = new byte[numPlayer][];
+      //Generate song data array
+      byte[][] gen = new byte[INSTRUMENTS.length][songLength];
+      
+      //Generate piano part
+      for(short i = 0; i < gen.length; i++){
+         
+      }
    }
    
 }
